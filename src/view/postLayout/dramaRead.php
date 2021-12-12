9f5605d95a0c2e0b817c3d91616cb95c11e04950
Maanuja Sutharsan <maanuja.sutharsan@etu.univ-paris1.fr>
2021-12-12 01:22:07 +0100

    touch up

--- src/view/postLayout/dramaRead.php
+++ src/view/postLayout/dramaRead.php
@@ -30,7 +30,7 @@
             ?>
             <div class="card-header"><?php echo $post->getTitle(); ?></div>
             <div class="card-body">
-                <h4 class="card-title">By <?php echo ' '.$author->getLastname().' '.$author->getFirstname() ?></h4>
+                <h4 class="card-title">By <?php echo ' '.$author->getFirstname().' '.$author->getLastname(); ?></h4>
             </div>
             <div class="clearfix">
                 <img src="assets/images/posts/<?php echo $post->getImage() ?>" class="col-md-6 float-md-end mb-3 ms-md-3" alt="...">
